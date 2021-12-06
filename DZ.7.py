40eb51ecb66fb0251a505805ba10c9a4ba864321
chAek <aektann89@gmail.com>
2021-12-06 22:28:05 +0200

    DZ7

---
+++ DZ.7.py
@@ -0,0 +1,13 @@
+while True:
+    list = (input("Введите строку: "))
+    src = (input("Введите слово которое нужно найти и заменить: "))
+    if src not in list:
+        print('Такого слова нет в вашей строке')
+        continue
+    elif src in list:
+        rpt = (input("Введите слово-замену: "))
+    print("Замена произведена:")
+    print(list.replace(src, rpt))
+    print('Количество замен:', list.count(src))
+
+
