6605df677bfe69af879f321b7da269271222f542
chAek <aektann89@gmail.com>
2021-12-06 22:26:16 +0200

    DZ7

--- DZ.4.py
+++ DZ.4.py
@@ -3,7 +3,7 @@ counter = 0
 for x in range(len(tickets)):
     st = str(x)
     st = '0' * (6 - len(st)) + st
-    if int(st[0]) + int(st[1]) + int(st[2]) == int(st[3]) + int(st[4]) + int(st[5]):
+                   if int(st[0]) + int(st[1]) + int(st[2]) == int(st[3]) + int(st[4]) + int(st[5]):
         counter += 1
 print('Cчастливых билетов:', counter)
 
